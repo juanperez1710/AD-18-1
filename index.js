ea4d73845a16ceccb5ea89f0f9599afa9022b40f
Juan Pérez Marcelo <junpemarc@gmail.com>
2025-09-27 16:11:07 -0600

    Se realizan actividades de la 1 a la 6

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const itemsContainer = document.querySelector("#list-items")
+const itemsContainer = document.querySelector("#list-items");
+const URL_API="https://reqres.in/api/unknown";
+const apiKey="reqres-free-v1";
+const STORAGE_KEY = "coloresGuardados"
 
 function addItem(item) {
   const colourCard = document.createElement("section")
@@ -26,14 +29,67 @@ function addItem(item) {
 
   const colourCardBreak = document.createElement("br")
   itemsContainer.append(colourCardBreak)
+
+}
+/**
+ * Tareas 1 y 2: 
+ * function fetchColorsList() {
+  fetch(URL_API)
+  .then((response)=> response.json())
+  .then((data)=>{
+    const colors=data.data;
+    colors.forEach((color)=>{
+      addItem(color);
+    })
+  }).catch((error)=>{
+    console.log(error)
+  })
+}
+ */
+
+/* Tarea 3: */
+function fetchColorsList() {
+  fetch(URL_API)
+  .then((response)=> response.json())
+  .then((data)=>{
+    const colors=data.data;
+    const colorsCadena=JSON.stringify(colors);
+    localStorage.setItem(STORAGE_KEY,colorsCadena);
+    colors.forEach((color)=>{
+      addItem(color);
+    })
+  }).catch((error)=>{
+    console.log(error);
+  })
 }
 
-function fetchColorsList() {
-  
+/** Tarea 4: */
+
+function renderColors(colorsArray){
+  itemsContainer.innerHTML="";
+  colorsArray.forEach((color)=>{
+    addItem(color);
+  })
 }
 
 function loadColorsFromStorage() {
-  
+  const colorsCadena=localStorage.getItem(STORAGE_KEY);
+  if (colorsCadena){
+    console.log("Colores almacenados en local storage");
+    const colors=JSON.parse(colorsCadena);
+    renderColors(colors);
+    return true;
+  }else{
+    console.log("No hay colores guardados en local storage");
+    return false;
+  }
+}
+
+/** Tarea 5 */
+function limpiarColores(){
+  localStorage.removeItem(STORAGE_KEY);
+  console.log("Colores eliminados del local storage");
+  itemsContainer.innerHTML="";
 }
 
 fetchColorsList()
